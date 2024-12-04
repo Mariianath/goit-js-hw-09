43f992d6f42d2118af28a79097f52a22293e67f1
Mariianath <93937744+Mariianath@users.noreply.github.com>
2024-12-04 13:52:41 +0000

    Deploying to gh-pages from @ Mariianath/goit-js-hw-09@22bc0c063ea14d3892b1a43ae15ec695f4601fba 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";import"./index.js";import{S as a}from"./assets/vendor-ca3668cb.js";new a(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250});
+//# sourceMappingURL=1-gallery.js.map
---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";import"./index.js";const e=document.getElementById("contact-form");e.addEventListener("submit",function(s){s.preventDefault();const t=e.email.value,l=e.message.value;if(!t||!l){alert("Please fill out all fields.");return}console.log("Email:",t),console.log("Message:",l),alert("Form submitted successfully!")});
+//# sourceMappingURL=2-form.js.map
