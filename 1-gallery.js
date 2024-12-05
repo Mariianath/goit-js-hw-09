2bbe5ff29a1ee9d848eeece6639bfd891bf165f2
Mariianath <93937744+Mariianath@users.noreply.github.com>
2024-12-05 14:38:22 +0000

    Deploying to gh-pages from @ Mariianath/goit-js-hw-09@a7be53048c9dd103199b71c1acce8ac9c82d7f49 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,2 +1,6 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";import"./index.js";import{S as a}from"./assets/vendor-ca3668cb.js";new a(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250});
+import"./assets/modulepreload-polyfill-ec808ebb.js";import"./index.js";import{S as i}from"./assets/vendor-ca3668cb.js";const g=[{preview:"small-image1.jpg",original:"large-image1.jpg",description:"Image 1"},{preview:"small-image2.jpg",original:"large-image2.jpg",description:"Image 2"},{preview:"small-image3.jpg",original:"large-image3.jpg",description:"Image 3"}],r=document.querySelector(".gallery"),m=g.map(({preview:a,original:e,description:l})=>`<li class="gallery-item">
+        <a class="gallery-link" href="${e}">
+          <img class="gallery-image" src="${a}" alt="${l}" />
+        </a>
+      </li>`).join("");r.innerHTML=m;new i(".gallery a",{captionsData:"alt",captionDelay:250});
 //# sourceMappingURL=1-gallery.js.map
